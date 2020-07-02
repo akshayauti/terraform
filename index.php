63ed2c2c7fbac1d930541683c3b90664e46f37ed
Akshay Auti <36589794+akshayauti@users.noreply.github.com>
2020-07-02 16:23:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title> My Website </head>
+    <meta
+      name="description"
+      content="Web site created to test terraform on aws"
+    />
+  </head>
+  <body>
+    <center><h1>Welcome to my site <h1></center>
+    <pre>
+    
+      `ifconfig`
+      
+    </pre>
+    
+  </body>
+</html>
